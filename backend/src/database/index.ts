faef8b2fb74d0f06621885ff9b48701e888b5a74
Morteza Jalilifar <morteza.jalilifar@publicispixelpark.de>
2021-01-24 23:51:59 +0100

    Add env vars.

--- backend/src/database/index.ts
+++ backend/src/database/index.ts
@@ -1,12 +1,8 @@
 import { MongoClient } from 'mongodb';
 
-const MONGO_USER1 = {
-    name: 'nancologist',
-    pwd: '1BgmNOzX8pWh4Ryf'
-}
 const DB_NAME = 'car24';
 
-const MONGO_URI = `mongodb+srv://${MONGO_USER1.name}:${MONGO_USER1.pwd}@cluster0.sgb6o.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`
+const MONGO_URI = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PWD}@cluster0.sgb6o.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`
 
 export const client = new MongoClient(
     MONGO_URI, 
